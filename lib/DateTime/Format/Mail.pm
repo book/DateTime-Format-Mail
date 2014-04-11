94ad9678feace14ff33d7ef4be33600249709fc0
Philippe Bruhat (BooK) <book@cpan.org>
2014-04-11 20:26:41 +0200

    $VERSION is also handled by dzil

--- lib/DateTime/Format/Mail.pm
+++ lib/DateTime/Format/Mail.pm
@@ -8,8 +8,6 @@ use DateTime 0.1705;
 use Params::Validate qw( validate validate_pos SCALAR );
 use vars qw( $VERSION );
 
-$VERSION = '0.3001';
-
 my %validations = (
     year_cutoff =>  {
         type => SCALAR,
